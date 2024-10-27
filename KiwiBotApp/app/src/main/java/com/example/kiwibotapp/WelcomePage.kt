73c7173da6553bbd13ca4c6db2dfe83eda0dd66c
kachup1 <emailkarlahere@gmail.com>
2024-10-26 20:34:36 -0700

    fixed config issues

---
+++ KiwiBotApp/app/src/main/java/com/example/kiwibotapp/WelcomePage.kt
@@ -0,0 +1,28 @@
+package com.example.kiwibotapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+
+class WelcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+
+        // Ensure the 'main' view exists before using it
+        val mainView = findViewById<View>(R.id.main)
+        if (mainView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        } else {
+            // Log an error if the view with id 'main' is not found
+            Log.e("WelcomePage", "'main' view not found in activity_welcome_page.xml")
+        }
+    }
+}
